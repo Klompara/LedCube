51611cbaa1f6c3b8c2ea00c901fd170089e20462
Michael <Michael@Michael-Pc>
2018-08-30 23:42:17 +0200

    Helix

--- src/bll/animation/Helix.java
+++ src/bll/animation/Helix.java
@@ -5,18 +5,19 @@ import java.awt.Color;
 import bll.SerialLED;
 
 public class Helix extends Animation {
-	private Color b = new Color(0, 0, 0);
-	private Color g = new Color(0, 255, 0);
 	int counter = 0, i = 1;
 
-	private int[][] circle = { { 0, 2 }, { 0, 3 }, { 0, 4 }, { 0, 5 }, { 1, 1 }, { 1, 6 }, { 2, 0 }, { 2, 7 }, { 3, 0 },
-			{ 3, 7 }, { 4, 0 }, { 4, 7 }, { 5, 0 }, { 5, 7 }, { 6, 1 }, { 6, 6 }, { 7, 2 }, { 7, 3 }, { 7, 4 },
-			{ 7, 5 } };
+	private int[][] circle = { { 0, 2 }, { 0, 3 }, { 0, 4 }, { 0, 5 }, { 1, 6 }, { 2, 7 }, { 3, 7 }, { 4, 7 }, { 5, 7 },
+			{ 6, 6 }, { 7, 5 }, { 7, 4 }, { 7, 3 }, { 7, 2 }, { 6, 1 }, { 5, 0 }, { 4, 0 }, { 3, 0 }, { 2, 0 },
+			{ 1, 1 } };
 
 	public void tick(SerialLED[][][] leds) {
 		counter += i;
-		for (int i = 0; i < circle.length; i++) {
-			leds[circle[i][0]][circle[i][1]][i / 3].setC(new Color(255, 0, 0));
+		for (int e = 0; e < 3; e++) {
+			for (int i = 0; i < circle.length; i++) {
+				leds[circle[i][0]][circle[i][1]][i / 4 + e * 4].setC(new Color(255, 0, 0));
+			}
 		}
+
 	}
 }
