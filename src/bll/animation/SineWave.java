5d6b7157e67dd85d147874de205617ea3ada933f
Mathias <Mathias@DESKTOP-9M92U0P>
2018-08-30 22:15:58 +0200

    Added Animation SineWave

---
+++ src/bll/animation/SineWave.java
@@ -0,0 +1,25 @@
+package bll.animation;
+
+import java.awt.Color;
+
+import app.Start;
+import bll.SerialLED;
+
+public class SineWave extends Animation{
+	
+	private int xcounter = -1, ycounter = -1, zcounter = -1;
+	private float counter = 0, i = 0.2f;
+	@Override
+	public void tick(SerialLED[][][] leds) {
+		
+		counter += i;
+		if ((counter == 7 && i > 0) || (counter == 0 && i < 0))
+			i *= -1;
+		
+		for (int x = 0; x < Start.cubeSize; x++) {
+			int y = (int) ((int)3*Math.sin(2*x-counter)) + 3;
+			leds[x][y][5].setC(new Color(255,0,0));
+		}
+	}
+
+}
